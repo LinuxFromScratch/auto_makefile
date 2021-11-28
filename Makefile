6ea883be42081b984f5a5fd358f6dc191c95c695
leon <372588334@qq.com>
2021-11-28 07:54:52 -0800

    使用 glib/auto_make.sh 脚本来编译链接.

--- Makefile
+++ Makefile
@@ -172,7 +172,7 @@ build_3th: FORCE
 
 objs := init/main.o
 
-all:  $(dirs) build_comms_static_lib FORCE
+all:  build_comms_static_lib FORCE
 
 test_dirs := sample/
 test_dirs := ${patsubst %/,%,$(filter %/, $(test_dirs))}
--- glib/auto_make.sh
+++ glib/auto_make.sh
@@ -49,6 +49,8 @@ auto_make_zlib()
 	
 	CMD [$FUNCNAME-$LINENO] "host=$1 install_path=$2"
 	tar -zxf zlib-1.2.11.tar.gz -C ./ 
+
+	export CFLAGS="-fPIC -Wall -Wno-format -fno-strict-aliasing -O2" 
 	if [ "$1" == "arm-hisiv300-linux" ] ;then	
 		export CC=$1-gcc
 	elif [ "$1" == "arm-hisiv400-linux" ] ;then		
@@ -61,7 +63,8 @@ auto_make_zlib()
 	fi
 	
 	cd zlib-1.2.11 && ./configure --static --enable-shared --prefix=$2 \
-	&& make && make install
+	&& make -j 8 && make install
+	cd -
 }
 
 auto_clean_zlib()
@@ -101,7 +104,8 @@ auto_make_libffi()
 		exit 1
 	fi
 	
-	make && make install
+	make -j 8 && make install
+	cd -
 }
 
 auto_clean_libffi()
@@ -111,17 +115,85 @@ auto_clean_libffi()
 }
 
 ######################################################################
+# auto make/clean glib
+######################################################################
+#$1 : host
+#$2 : install_path
+auto_make_glib()
+{
+	ENV_VAR_UNSET
+	
+	CMD [$FUNCNAME-$LINENO] "host=$1 install_path=$2"
+	tar -zxf glib-2.40.2.tar.gz -C ./ 
+	cd glib-2.40.2
+	options="--enable-silent-rules --enable-static --enable-shared --with-pcre=internal --disable-gtk-doc-html --disable-man "
+	cache_options="glib_cv_stack_grows=no glib_cv_uscore=no ac_cv_func_posix_getpwuid_r=no ac_cv_func_posix_getgrgid_r=no"
+	
+	if [ "$1" == "arm-hisiv300-linux" ] ;then	
+		./configure  ${options} ${cache_options} --host=$host --prefix=$2 \
+		CC=$1-gcc \
+		CFLAGS="-fPIC -Wall -Wno-format -fno-strict-aliasing -O2 -Wno-format-nonliteral -Wno-format-overflow  -Wno-format-security " \
+		ZLIB_CFLAGS="-I $2/include" ZLIB_LIBS="$2/lib/libz.a" \
+		LIBFFI_CFLAGS="-I$2lib/libffi-3.2.1/include" LIBFFI_LIBS="$2/lib/libffi.la"
+	elif [ "$1" == "arm-hisiv400-linux" ] ;then		
+		./configure  ${options} ${cache_options} --host=$host --prefix=$2 \
+		CC=$1-gcc \
+		CFLAGS="-fPIC -Wall -Wno-format -fno-strict-aliasing -O2 -Wno-format-nonliteral -Wno-format-overflow  -Wno-format-security" \
+		ZLIB_CFLAGS="-I $2/include" ZLIB_LIBS="$2/lib/libz.a" \
+		LIBFFI_CFLAGS="-I$2lib/libffi-3.2.1/include" LIBFFI_LIBS="$2/lib/libffi.la"
+	elif [ "$1" == "x86" ] ;then		
+		./configure  ${options} ${cache_options} --prefix=$2 \
+		CC=gcc \
+		CFLAGS="-fPIC -Wall -Wno-format -fno-strict-aliasing -O2 -Wno-format-nonliteral -Wno-format-overflow  -Wno-format-security" \
+		ZLIB_CFLAGS="-I $2/include" ZLIB_LIBS="$2/lib/libz.a" \
+		LIBFFI_CFLAGS="-I $2/lib/libffi-3.2.1/include" LIBFFI_LIBS="$2/lib/libffi.la"
+	else
+		ERR [$FUNCNAME-$LINENO] "host: $1 unsupport..."
+		exit 1
+	fi
+	
+	make -j 8 && make install
+	cd -
+}
+
+auto_clean_glib()
+{
+	CMD [$FUNCNAME-$LINENO] "rm -fr glib-2.40.2"
+	rm -fr glib-2.40.2/
+}
+
+######################################################################
+# Merge all libs
+######################################################################
+merge_mutil_libs()
+{
+	set -e
+	ld -r -o $install_path/lib/libAUTOMAKE.a  -L $install_path/lib --whole-archive `ls $install_path/lib/*.a` --no-whole-archive
+	exit 0
+}
+
+
+######################################################################
 # do work
 ######################################################################
 if [ "$action" == "make" ] ;then
+	set -e
+	auto_make_zlib $host $install_path
 	auto_make_libffi $host $install_path
+	auto_make_glib $host $install_path
+	merge_mutil_libs
 	exit 0
 	
 elif [ "$action" == "clean" ] ;then
 	auto_clean_zlib
+	auto_clean_libffi
+	auto_clean_glib
 	exit 0
 else
 	ERR [$FUNCNAME-$LINENO] "unknow command..."
 	exit 0
 fi	
+
+
+
 
